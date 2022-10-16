969ead8c1d6f1e3a28021b64cb56587a9ebde2aa
linuxdaemon <linuxdaemonirc@gmail.com>
2022-10-15 23:57:54 -0400

    Add more logging to crafter

--- me-colony-crafter/init.lua
+++ me-colony-crafter/init.lua
@@ -19,13 +19,16 @@ local function handle_request(request)
     local exported = false
     for _, item in ipairs(request.items) do
         if stash_items[item.name] then
+            print(item.name .. " still in stash")
             return false
         end
         local me_item = translate_item(item, request.count)
         if me.isItemCrafting(me_item) then
+            print(item.name .. " is still crafting")
             tried_craft[me_item] = nil
             return false
         elseif me.getItem(me_item) ~= nil then
+            --print(string.format("Found %s in storage", item.name))
             exportable[#exportable + 1] = me_item
         elseif me.isItemCraftable(me_item) then
             craftable[#craftable + 1] = me_item
@@ -33,15 +36,20 @@ local function handle_request(request)
     end
     if #exportable > 0 then
         local item = exportable[1]
+        print(string.format("Found %s in storage", item.name))
         tried_craft[item] = nil
         me.exportItem(item, "UP")
     elseif #craftable > 0 then
+        print(string.format("%s is craftable, crafting"))
         if tried_craft[item] then
             print("Already tried crafting " .. item.name)
         end
         local item = craftable[1]
         tried_craft[item] = true
-        me.craftItem(item)
+        ok, msg = me.craftItem(item)
+        if not ok then
+            print(string.format("Failed to craft %s: %s", item.name, msg))
+        end
     else
         print("Need " .. request.name .. " for " .. request.target)
     end

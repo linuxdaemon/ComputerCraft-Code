c5bc3f5a7eea084ee7591abe49c7afa52a4fb230
linuxdaemon <linuxdaemonirc@gmail.com>
2022-10-15 23:43:56 -0400

    Add autocrafting code

---
+++ me-colony-crafter/init.lua
@@ -0,0 +1,64 @@
+local me = peripheral.find("meBridge")
+local colony = peripheral.find("colonyIntegrator")
+local stash = peripheral.find("minecolonies:stash")
+
+local stash_items = {}
+
+local function translate_item(item, count)
+    return {
+        name = item.name,
+        count = count
+    }
+end
+
+local tried_craft = {}
+
+local function handle_request(request)
+    local craftable = {}
+    local exportable = {}
+    local exported = false
+    for _, item in ipairs(request.items) do
+        if stash_items[item.name] then
+            return false
+        end
+        local me_item = translate_item(item, request.count)
+        if me.isItemCrafting(me_item) then
+            tried_craft[me_item] = nil
+            return false
+        elseif me.getItem(me_item) ~= nil then
+            exportable[#exportable + 1] = me_item
+        elseif me.isItemCraftable(me_item) then
+            craftable[#craftable + 1] = me_item
+        end
+    end
+    if #exportable > 0 then
+        local item = exportable[1]
+        tried_craft[item] = nil
+        me.exportItem(item, "UP")
+    elseif #craftable > 0 then
+        if tried_craft[item] then
+            print("Already tried crafting " .. item.name)
+        end
+        local item = craftable[1]
+        tried_craft[item] = true
+        me.craftItem(item)
+    else
+        print("Need " .. request.name .. " for " .. request.target)
+    end
+end
+
+local function main_loop()
+    stash_items = {}
+    for _, item in ipairs(stash.list()) do
+        stash_items[item.name] = true
+    end
+    requests = colony.getRequests()
+    for _, request in ipairs(requests) do
+        handle_request(request)
+    end
+end
+
+while true do
+    main_loop()
+    os.sleep(30)
+end
